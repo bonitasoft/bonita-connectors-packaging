b7f3a7d500735048d3504081b421a70434a2406e
Frederic Bouquet <frederic.bouquet@bonitasoft.com>
2013-06-26 16:38:03 +0000

    Prepared AS tests for connectors

---
+++ bonita-connectors-tomcat-integration-tests/src/test/java/astests/ProcessWithEmailConnectorAndDocumentTest.java
@@ -0,0 +1,20 @@
+package astests;
+
+import org.bonitasoft.engine.api.APIAccessor;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ProcessWithEmailConnectorAndDocumentTest {
+    APIAccessor apiAccessor;
+
+    @BeforeClass
+    public void before() {
+        // TODO : initialize apiAccessor
+    }
+
+    @Test
+    public void testBasicProcess() {
+
+
+    }
+}
